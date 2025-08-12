e3a4c7c387ca2a0088bca0e49779e43b3be641f7
asanka <nmsan88@gmail.com>
2025-08-12 15:06:28 +0530

    Admin Validation fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@example.com') || str_ends_with($this->email, '@admin.com');
+        return true;
     }
 
     /**
